24baa30d724d41b62cd231679daf72e5d0943142
Hasib <hasibnon@gmail.com>
2026-05-29 00:55:03 +0600

    centering my sign-up page

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,10 @@
-import { SignUp } from '@clerk/nextjs'
+import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp />
+  return (
+    <div  className="flex w-screen h-screen justify-center items-center " >
+      
+      <SignUp />
+    </div>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,23 @@
+import { UserButton } from '@clerk/nextjs'
+import { User } from '@clerk/nextjs/server'
+import Link from 'next/link'
 import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className=' flex flex-col' >
+      <div className='flex w-screen justify-end p-8' >
+
+      <UserButton/>
+      </div>
+      <div className='flex text-center w-screen justify-center mt-40' >
+        <h1 className='text-4xl font-bold  text-white ' >Welcome    </h1>
+      </div>
+      <div className='mt-6 justify-center flex  text-green-500 font-bold' > 
+      <Link href={'/sign-up'}>signUp</Link>
+
+      </div>
+    </div>
   )
 }
 
