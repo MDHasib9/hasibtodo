4a7bd5bfa7f774d459a0fe40a6aa1a3393e63994
Hasib <hasibnon@gmail.com>
2026-05-29 09:08:33 +0600

    styling the todos route

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -1,6 +1,4 @@
-
-
-'use server'
+"use server";
 
 import { addTodos, deleteTodo } from "../actions";
 import { auth } from "@clerk/nextjs/server";
@@ -17,26 +15,24 @@ const page = async () => {
     orderBy: { createdAt: "desc" },
   });
   return (
-    <div>
+    <div className="w-screen flex-col h-screen ">
       {/* adding todo */}
-      <div>
+      <div className="w-screen flex pt-40 justify-center items-center ">
         <form action={addTodos}>
-          <input type="text" name="todoText" required />
-          <button type="submit">add</button>
-        </form>
+          <input placeholder="Enter a todo" className=" border border-gray-700 rounded-md py-1.5 px-6" type="text" name="todoText" required />
+          <button className="bg-blue-600 py-1 px-3 rounded-md mx-3" type="submit">add</button>
+        </form>{" "}
       </div>
-
-      <div>
+      <div className=" w-screen  ">
         {todos.map((todo) => (
-          <div key={todo.id} >
-            <p>{todo.text}</p>
+          <div className="w-screen flex pt-3 justify-center items-center">
+            <div className=" w-90 flex  justify-between p-4 rounded-lg  items-center bg-gray-600 overflow-auto "  key={todo.id}>
+              <p className="" >{todo.text}</p>
 
-            <form action={deleteTodo.bind(null, todo.id)}>
-
-
-
-                <button type="submit" >delete</button>
-            </form>
+              <form className="" action={deleteTodo.bind(null, todo.id)}>
+                <button className="bg-red-800 py-1 px-3 rounded-md  text-white" type="submit">delete</button>
+              </form>
+            </div>
           </div>
         ))}
       </div>
